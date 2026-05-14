a44a7beba6c8b3fb0a6dae91d37a6e6e3717c489
kaede96az <147464913+kaede96az@users.noreply.github.com>
2026-05-14 23:31:17 +0900

    Feature/add inoculation type (#119)
    * feat: 接種種類への対応機能を追加
    
    * refactor: タイポを修正

--- src/components/SelectItemsWithNumber.vue
+++ src/components/SelectItemsWithNumber.vue
@@ -3,8 +3,9 @@
 		v-model="values"
 		:items="items"
 		:label="label"
+		item-title="display_name"
+		item-value="value"
 		@update:model-value="searchTriggerFunc"
-		:item-props="itemProps"
 		hide-details="auto"
 		multiple
 		>
@@ -15,15 +16,17 @@
 						values = []
 						clearTriggerFunc()
 					}">全て選択解除</v-btn>
+					<slot name="help"></slot>
 				</template>
+				
 			</v-list-item>
+			
 			<v-divider class="mt-2"></v-divider>
 		</template>
 
 		<template v-slot:selection="{ item, index }">
 			<v-chip v-if="index < 2">
-				<span v-if="item.title == ''">（記載なし）</span>
-				<span v-else>{{ item.title }}</span>
+				<span>{{ item.title }}</span>
 			</v-chip>
 			<div v-if="index === 2" class="text-grey text-caption align-self-center">
 				<span v-if="values == undefined">
@@ -38,22 +41,16 @@
 </template>
 
 <script setup lang="ts">
+import type { SelectItem } from '@/types/SelectItem';
+
 defineProps<{
   label: string
   searchTriggerFunc: () => void
   clearTriggerFunc: () => void
 }>()
 
-const values = defineModel<string[]>('values')
-const items = defineModel<string[]>('items')
-
-const itemProps = (item: any) => {
-        return item == '' ? {
-			title: '（記載なし）',
-        } : {
-			title: item,
-		}
-}
+const values = defineModel<number[]>('values')
+const items = defineModel<SelectItem[]>('items')
 </script>
 
 <style scoped></style>
